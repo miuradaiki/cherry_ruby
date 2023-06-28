809cca22fc1b0cec2c297cce614a953973470df6
Daiki <daiki.miura@prum.jp>
2023-06-28 23:50:27 +0900

    add: 学習記録0628

--- chapter_5.rb
+++ chapter_5.rb
@@ -26,3 +26,21 @@ currencies.values #=> ["yen", "dollar", "rupee"]
 currencies = { "japan" => "yen", "us" => "dollar", "india" => "rupee"}
 currencies.has_key?(:japan) #=> true
 currencies.has_key?(:iutaly) #=> false
+
+# **でハッシュを展開させる
+h = { us: "doller", india: "rupee"}
+{ japan: "yen", **h} #=> { japan: "yen", us: "doller", india: "rupee"}
+
+# mergeを使っても同じ結果を得ることができる
+h = { us: "doller", india: "rupee"}
+{ japan: "yen" }.merge(h)
+
+# ハッシュから配列、配列からハッシュへ
+currencies = { japan: "yen", us: "doller", india: "rupee"}
+currencies.to_a
+
+array = [1, 2, 3, 4]
+# p array.to_h #=> ハッシュとして解析不能な場合はエラーとなる
+
+array = [[:japan, "yen"], [:us, "doller"], [:india, "rupee"]]
+# p array.to_h # => {:japan=>"yen", :us=>"doller", :india=>"rupee"}
