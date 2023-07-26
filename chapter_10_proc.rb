8d68b7bfad55fa1f020e98b24f6a2639a77ce2cd
Daiki <daiki.miura@prum.jp>
2023-07-26 23:36:54 +0900

    add: 学習記録0726

--- chapter_10_proc.rb
+++ chapter_10_proc.rb
@@ -52,3 +52,43 @@ greet(repeat_proc)
 # ー>構文またはlambdaメソッドでProcオブジェクトを作成する
 # ->(a, b) { a + b }
 # lambda { |a, b| a + b }
+
+# Procオブジェクトを実行するさまざまな方法
+add_proc = Proc.new { | a, b | a + b }
+
+# callメソッドを使う
+add_proc.call(10, 20) #=>30
+
+# yieldメソッドを使う
+add_proc.yield(10, 20) #=>30
+
+# .()を使う
+add_proc.(10, 20) #=>30
+
+# []を使う
+add_proc[10, 20] #=>30
+
+# ===を使って呼び出す
+# これで呼び出せる理由としては、case文のwhen節で使えるようにするため
+add_proc === [10, 20] #=>30
+
+# 上の例
+def judge age
+  # 20より大きければtrueを返すProcオブジェクト
+  adult = Proc.new { |n| n > 20}
+  # 20より小さければtrueを返すProcオブジェクト
+  child = Proc.new { |n| n < 20}
+end
+
+case age
+when adult
+  "大人です"
+when child
+  "子供です"
+else
+  "はたちです"
+end
+
+judge(25) #=>"大人です"
+judge(18) #=>"子供です"
+judge(20) #=>"はたちです"
